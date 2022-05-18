50500873003ca6105883e5e0f2443b6dc0680945
Sly <slymasterfunk@gmail.com>
2022-05-18 09:06:08 +0900

    qrc added

--- main.js
+++ main.js
@@ -140,6 +140,22 @@ function update(){
 update()
 
 // ---------------------- //
+//        QR Expand       //
+// ---------------------- //
+$(".qrc").click(function (event) {
+  if ($(this).hasClass("expand")) {
+    $(this).removeClass("expand").addClass("shrink");
+  } else {
+    if ($(this).hasClass("shrink")) {
+      $(this).removeClass("shrink").addClass("expand");
+    } else {
+      $(this).addClass("expand");
+    }
+  }
+});
+
+
+// ---------------------- //
 //          Menu          //
 // ---------------------- //
 
